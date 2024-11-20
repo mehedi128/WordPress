9fe78911f0fa44cbe00e2265b3ecda88af8019f5
mehedi128 <106508951+mehedi128@users.noreply.github.com>
2024-11-20 13:54:16 +0600

    Create custom_tag.php

---
+++ inc/custom_tag.php
@@ -0,0 +1,104 @@
+/**
+* Sanitize SVG markup for front-end display.
+*
+* @param  string $svg SVG markup to sanitize.
+* @return string 	  Sanitized markup.
+*/
+function aidzone_kses( $aidzone_custom_tag = '' ) {
+	$aidzone_allowed_html = [
+        'svg' => array(
+            'class' => true,
+            'aria-hidden' => true,
+            'aria-labelledby' => true,
+            'role' => true,
+            'xmlns' => true,
+            'width' => true,
+            'height' => true,
+            'viewbox' => true, // <= Must be lower case!
+        ),
+        'path'  => array( 
+            'd' => true, 
+            'fill' => true,  
+            'stroke' => true,  
+            'stroke-width' => true,  
+            'stroke-linecap' => true,  
+            'stroke-linejoin' => true,  
+            'opacity' => true,  
+        ),
+		'a' => [
+			'class'    => [],
+			'href'    => [],
+			'title'    => [],
+			'target'    => [],
+			'rel'    => [],
+		],
+         'b' => [],
+         'blockquote'  =>  [
+            'cite' => [],
+         ],
+         'cite'                      => [
+            'title' => [],
+         ],
+         'code'                      => [],
+         'del'                    => [
+            'datetime'   => [],
+            'title'      => [],
+        ],
+         'dd'                     => [],
+         'div'                    => [
+            'class'   => [],
+            'title'   => [],
+            'style'   => [],
+         ],
+         'dl'                     => [],
+         'dt'                     => [],
+         'em'                     => [],
+         'h1'                     => [],
+         'h2'                     => [],
+         'h3'                     => [],
+         'h4'                     => [],
+         'h5'                     => [],
+         'h6'                     => [],
+         'i'                         => [
+            'class' => [],
+         ],
+         'img'                    => [
+            'alt'  => [],
+            'class'   => [],
+            'height' => [],
+            'src'  => [],
+            'width'   => [],
+         ],
+         'li'                     => array(
+            'class' => array(),
+         ),
+         'ol'                     => array(
+            'class' => array(),
+         ),
+         'p'                         => array(
+            'class' => array(),
+         ),
+         'q'                         => array(
+            'cite'    => array(),
+            'title'   => array(),
+         ),
+         'span'                      => array(
+            'class'   => array(),
+            'title'   => array(),
+            'style'   => array(),
+         ),
+         'iframe'                 => array(
+            'width'         => array(),
+            'height'     => array(),
+            'scrolling'     => array(),
+            'frameborder'   => array(),
+            'allow'         => array(),
+            'src'        => array(),
+         ),
+         'strike'                 => array(),
+         'br'                     => array(),
+         'strong'                 => array(),
+	];
+
+	return wp_kses( $aidzone_custom_tag, $aidzone_allowed_html );
+}
